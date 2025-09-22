c682de0c4d349a747f963751e96bd243531785b4
Aayush Raj <aayushraj2378@gmail.com>
2025-09-22 12:10:06 +0530

    Navbar: always show Register; keep Login/Logout conditional

--- frontend/src/components/Main/Header.js
+++ frontend/src/components/Main/Header.js
@@ -44,6 +44,10 @@ const Header = ({ searchText, setSearchText }) => {
                                 <Link to="/" className="nav-link">Home</Link>
                             </li>
 
+                            <li className="nav-item">
+                                <Link to="/register" className="nav-link">Register</Link>
+                            </li>
+
                             {user ? (
                                 <li className="nav-item">
                                     <button
@@ -54,14 +58,9 @@ const Header = ({ searchText, setSearchText }) => {
                                     </button>
                                 </li>
                             ) : (
-                                <>
-                                    <li className="nav-item">
-                                        <Link to="/register" className="nav-link">Register</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/login" className="nav-link">Login</Link>
-                                    </li>
-                                </>
+                                <li className="nav-item">
+                                    <Link to="/login" className="nav-link">Login</Link>
+                                </li>
                             )}
                         </ul>
 
